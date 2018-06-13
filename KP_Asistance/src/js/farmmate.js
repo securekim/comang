5e6066e03ad95089ee5d7933d15fa36eb5b7dda9
PickMeD <nii_p00ng@naver.com>
2018-06-13 17:02:12 +0900

    씨바

--- KP_Asistance/src/js/farmmate.js
+++ KP_Asistance/src/js/farmmate.js
@@ -6,47 +6,77 @@ let markers
 function initMap() {
   refleshSideBar()
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat:myLat,
-      lng:myLng
-    },
+    center: locationss[0],
     zoom: 16
   })
-  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   //내위치 나타내기
   new google.maps.Marker({
-    position: new google.maps.LatLng(myLat, myLng),
+    position: new google.maps.LatLng(locationss[0].lat, locationss[0].lng),
     map,
     draggable: false,
     icon: 'http://maps.google.com/mapfiles/ms/micons/man.png'
   })
 
 
+
+
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var icon = {
+    url: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678134-sign-check-512.png',
+    scaledSize: new google.maps.Size(30, 30), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+  }
+
+  for(var i = 0 ; i < locationss.length ; i++){
+    var marker = new google.maps.Marker({
+      position: locationss[i],
+      icon: icon,
+      map:map
+    })
+    var infowindow = new google.maps.InfoWindow()
+
+    var content = '1111'
+
+    google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
+      return function() {
+        infowindow.setContent(content);
+        infowindow.open(map,marker);
+      }
+    })(marker,content,infowindow))
+  }
+
+
+
+
   //마커들 등록
   markers = locationss.map((location, i) => {
      var marker = new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length]
-    })
-
-    google.maps.event.addListener(marker, 'click', (evt) => {
-      infowindow.open(map, marker)
-    })
-
-    var contentString = '123123'
-
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
+       position: location,
+       icon: icon
     })
 
     return marker
   })
 
+  var flightPath = new google.maps.Polyline({
+    path: loads,
+    geodesic: true,
+    strokeColor: '#f86c6b',
+    strokeOpacity: 1.0,
+    strokeWeight: 5
+  });
+
+  flightPath.setMap(map);
 
 
+  var endLocation = new google.maps.Polyline({
+    path: endLocations,
+    geodesic: true,
+    strokeColor: '#4dbd74',
+    strokeOpacity: 1.0,
+    strokeWeight: 5
+  });
 
-  var markerCluster = new MarkerClusterer(map, markers,
-    {
-      imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    })
+  endLocation.setMap(map);
 }
